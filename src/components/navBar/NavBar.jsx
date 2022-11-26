1ad5f20fa49fc2125f70ce2b6a1028726db598a4
Luis Anaya <delarex121@gmail.com>
2022-11-26 18:03:13 +0100

    primera preentrega proyecto final

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -10,7 +10,7 @@ function Navegacion() {
         <>
             <Navbar bg="primary" variant="dark" expand="xl">
                 <Container>
-                    <Navbar.Brand href="#home">BAD</Navbar.Brand>
+                    <Navbar.Brand href="#home"><img src="https://res.cloudinary.com/dt1bumvty/image/upload/v1669481743/Untitled_design_5_rvfynv.png" alt="" width="50" height="50" /></Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="#todos">Todos</Nav.Link>
                         <Nav.Link href="#sencillos">Estilos Sencillos</Nav.Link>
