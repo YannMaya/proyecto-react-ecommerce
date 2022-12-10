8e9fcd1d55229bca3938ae39f890bb8e514adf73
Luis Anaya <delarex121@gmail.com>
2022-12-10 18:15:03 +0100

    termine todo hasta antes de volver a intentar hacer lo de item yitemlist

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,20 +1,49 @@
-import {useState } from "react"
+import { useState, useEffect } from "react"
 import ItemCount from '../itemCount/ItemCount'
 import "./ItemListContainer.css"
+import ProductCard from "../productCard/ProductCard";
+import { productos } from "../../productos"; /* ./ misma altura del archivo */
 
-const ItemListContainer = ( { greetings } ) => {
+const ItemListContainer = ({ greetings }) => {
 
-    const [modoOscuro, setModoOscuro] = useState(false)
+    const [modoOscuro, setModoOscuro] = useState(false);
+    const [items, setItems] = useState([]); /* inicializar con el tipo de dato a lo que pense que vamos a recibir */
 
-    const cambiarModoOscuro = ()=>{
+    const cambiarModoOscuro = () => {
         setModoOscuro(!modoOscuro)
     }
+
+    useEffect(() => {
+        const peticion = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(productos); /* puede resolver un objeto, arreglo, numero*/
+            }, 2000);
+        });
+
+        peticion
+            .then((res) => {
+                setItems(res); /* setItems me va a setear mi estado con lo que venga en la respuesta */
+            })
+            .catch((err) => {
+                console.log("se rechazo");
+            });
+
+        console.log("se hizo la peticion");
+    }, []);
+    console.log(items)
 
     return (
         <div className={modoOscuro ? "container-items-dark" : "container-items-light"}>
             <h2>{greetings}</h2>
-            <ItemCount initial={1} stock={10}/>
+            <ItemCount initial={1} stock={10} />
             <button onClick={cambiarModoOscuro}>{modoOscuro ? "🌙" : "⭐"}</button>
+            {
+                items.map((elemento)=>{
+                    return ( /* todo lo que voy a retornar esta dentro del parentesis */
+                        <ProductCard key={elemento.id} elemento={elemento}/>
+                    );    
+            })
+            }
         </div>
     )
 }
---
+++ src/productos.js
@@ -0,0 +1,36 @@
+export const productos = [
+
+    {
+        id: 1,
+        name: "green nails",
+        price: 500,
+        stock: 5,
+        description: "beautiful green nails",
+        img: "https://res.cloudinary.com/dt1bumvty/image/upload/v1670099607/2_bvy4gx.png"
+    },
+    {
+        id: 2,
+        name: "black nails",
+        price: 500,
+        stock: 5,
+        description: "beautifully done",
+        img: "https://res.cloudinary.com/dt1bumvty/image/upload/v1670099607/1_tkqlnz.png"
+    },
+    {
+        id: 3,
+        name: "blue nails",
+        price: 900,
+        stock: 5,
+        description: "for every occasion",
+        img:"https://res.cloudinary.com/dt1bumvty/image/upload/v1670099607/3_bjtdyn.png"
+    },
+    {
+        id: 4,
+        name: "metal nails",
+        price: 800,
+        stock: 5,
+        description: "Zapatillas de ultima generacion para el deporte",
+        img: "https://res.cloudinary.com/dt1bumvty/image/upload/v1670099607/5_njwkvv.png"
+    }
+
+]
